922fa4396ca084296277be87a8dac30aa35ccf2f
dk <daniel.kroeni@gmail.com>
2010-08-24 22:34:02 +0800

    fixed sbt project so it builds against 2.8.0

--- project/build/Scopt.scala
+++ project/build/Scopt.scala
@@ -3,7 +3,7 @@ import sbt._
 class ScoptProject(info: ProjectInfo) extends DefaultProject(info) {
 
   val newReleaseToolsRepository = "Scala Tools Repository" at "http://nexus.scala-tools.org/content/repositories/snapshots/"
-  val scalatest = "org.scalatest" % "scalatest" % "1.0.1-for-scala-2.8.0.Beta1-with-test-interfaces-0.3-SNAPSHOT" % "test"
+  val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
   val junit = "junit" % "junit" % "4.7" % "test"
   
   val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
